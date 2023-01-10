565328df36d3e5b98e05b9ae25037a66115dcd9c
Benito Geordie <benito@thirdai.com>
2023-01-10 17:02:37 -0600

    todo

--- race/test_race_classwise_torch.py
+++ race/test_race_classwise_torch.py
@@ -140,4 +140,4 @@ def test_scores_account_for_alpha():
     assert A_average_score > 5 * B_average_score
 
 
-# - Repeat the above tests for classwise inserts
+# TODO(Geordie): Repeat the above tests for classwise inserts

b0cee88c0c90887bff8f5001d2153efbffe3409b
Benito Geordie <benito@thirdai.com>
2023-01-10 15:32:30 -0600

    populous neighborhood -> higher score test

--- race/test_race_classwise_torch.py
+++ race/test_race_classwise_torch.py
@@ -24,34 +24,34 @@ RACE_PARAMS = {
         "srp_torch": {
             "dimension": INPUT_DIMENSION,
             "num_bits": 2,
-            "num_hash" : 5,
+            "num_hash" : 10,
             "seed": 101,
             "device_id": -1,
         }
     }
 }
 
+def random_dataset(num_samples, mean=0.0, scale=1.0):
+    """Creates a set of normally distributed vectors around a mean
+    and converts it to a torch float tensor.
+    """
+    dataset = np.random.normal(
+        loc=mean, 
+        scale=scale, 
+        size=(num_samples, INPUT_DIMENSION))
+    return torch.from_numpy(dataset).type(torch.FloatTensor)
 
 def test_rows_independent():
     """Checks that each row in the RACE sketch uses an independent hash 
     function. We do this by asserting that none of the samples we pass into 
     race gets the same count from every row of the sketch.
     """
-    race = Race_Classwise_torch(RACE_PARAMS)
     
     num_samples = 1000
+    dataset = random_dataset(num_samples)
+    alpha = torch.ones(size=(num_samples,))
     
-    # Normally distributed random dataset
-    dataset = np.random.normal(
-        loc=0.0, 
-        scale=1.0, 
-        size=(num_samples, INPUT_DIMENSION))
-    dataset = torch.from_numpy(dataset)
-    dataset = dataset.type(torch.FloatTensor)
-    
-    # Alpha is 1.0 for all datasets
-    alpha = torch.ones(size=(num_samples,)).type(torch.FloatTensor)
-    
+    race = Race_Classwise_torch(RACE_PARAMS)
     race.add(dataset, alpha)
 
     # Returns a num_hashes x num_samples matrix;
@@ -74,9 +74,36 @@ def test_rows_independent():
     assert not any(equal_counts_across_race_rows)
 
 
+def test_more_populous_neighborhood_gets_higher_score():
+    """Checks that RACE gives a higher score to elements that have more similar
+    elements than elements with fewer similar elements.
+    """
+    # Neighborhood A has a much larger population than neighborhood B
+    A_population = 1000
+    B_population = 100
+    
+    # Means are 180˚ apart; their hashes will never collide.
+    A_mean = np.random.normal(0.0, scale=1.0, size=(INPUT_DIMENSION,))
+    B_mean = -A_mean
+    
+    # Use smaller scale to generate neighborhood so that neighbors are much
+    # closer to their respective centers than elements of the other neighborhood.
+    neighborhood_A = random_dataset(A_population, A_mean, scale=0.5)
+    neighborhood_B = random_dataset(B_population, B_mean, scale=0.5)
+
+    race = Race_Classwise_torch(RACE_PARAMS)
+    race.add(neighborhood_A, alpha=torch.ones(size=(A_population,)))
+    race.add(neighborhood_B, alpha=torch.ones(size=(B_population,)))
+
+    A_average_score = torch.mean(race.query(neighborhood_A))
+    B_average_score = torch.mean(race.query(neighborhood_B))
+    # Factor of 5 instead of 10 because there may be hash collisions between
+    # elements of the different neighborhoods.
+    assert A_average_score > 5 * B_average_score
 
 
-# - Score is higher when there are more similar elements
+
+
 # - Test weights are incorporated in score
 # - Scores are as expected? But what is the expectation?
 # - Repeat the above tests for classwise inserts
